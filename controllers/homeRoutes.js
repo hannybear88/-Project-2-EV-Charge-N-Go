4855346886181ef80b499ebfb8a38572c6c98624
heidiwu3388 <heidiwu3388@yahoo.com>
2023-01-13 00:31:07 -0800

    fix a bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,13 +38,4 @@ router.get('/logout'), async (req,res) => {
   res.render('logout')
 };
 
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-
-
 module.exports = router;
