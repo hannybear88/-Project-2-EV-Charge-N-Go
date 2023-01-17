be8cadf4eacae1cdbda085b2dd3e2a174af3d4e4
Triana F <trianadeguzman@gmail.com>
2023-01-16 21:59:53 -0800

    created seed data for reservation table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Station } = require('../models');
+const { User, Station, Reservation } = require('../models');
 
 const userData = require('./userData.json');
 const stationData = require('./stationData.json');
+const reservationData = require('./reservationData.json');
 
 seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -18,7 +19,13 @@ seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  
+
+  // seed data for the table "reservation"
+  const reservations = await Reservation.bulkCreate(reservationData, {
+    individualHooks: true,
+    returning: true,
+  })
+
   process.exit(0);
 };
 
